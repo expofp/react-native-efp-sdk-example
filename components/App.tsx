3af23b73af6b8e03f356f5858ac37bff1a262a98
kda <mail@kda.name>
2025-10-13 19:10:36 +0300

    chore: refactor

--- components/App.tsx
+++ components/App.tsx
@@ -1,25 +1,84 @@
-import { StyleSheet } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import ExpoFpView from "./ExpoFpView";
-import Overlay from "./Overlay";
+import { ExpofpView, type ExpofpViewMethods } from "@expofp/expofp-sdk-rn";
+import { useCallback, useRef, useState } from "react";
+import { Button, StyleSheet, View } from "react-native";
+
+const FP = ["vlg-routes-01", "demo"];
 
 export default function App() {
+  const apiRef = useRef<ExpofpViewMethods>(null);
+  const [idx, setIdx] = useState(0);
+
+  const [expoKey, setExpoKey] = useState(FP[idx]);
+  const switchKey = useCallback(() => {
+    const nextIdx = (idx + 1) % FP.length;
+    setIdx(nextIdx);
+    setExpoKey(FP[nextIdx]);
+  }, [idx]);
+
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.view}>
-        <ExpoFpView style={styles.map} url="https://demo.expofp.com/" />
-        <Overlay />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={styles.view}>
+      <ExpofpView
+        ref={apiRef}
+        style={styles.view}
+        url={`https://${expoKey}.expofp.com/`}
+        onBoothDidClick={(e: any) => {
+          console.log("MAP 1: Booth clicked:", e);
+        }}
+      />
+      <ExpofpView
+        expoKey="demo"
+        onBoothDidClick={(e: any) => {
+          console.log("MAP 2: Booth clicked:", e);
+        }}
+      />
+      <View style={styles.buttons}>
+        <Button title="Next" onPress={switchKey} />
+        <Button
+          title="+"
+          onPress={() => {
+            console.log(
+              "🔶 [App] + button pressed, apiRef.current:",
+              apiRef.current,
+            );
+            apiRef.current?.zoomIn();
+          }}
+        />
+        <Button
+          title="-"
+          onPress={() => {
+            console.log(
+              "🔶 [App] - button pressed, apiRef.current:",
+              apiRef.current,
+            );
+            apiRef.current?.zoomOut();
+          }}
+        />
+        <Button
+          title="Fit Bounds"
+          onPress={() => {
+            console.log(
+              "🔶 [App] Fit Bounds button pressed, apiRef.current:",
+              apiRef.current,
+            );
+            apiRef.current?.fitBounds();
+          }}
+        />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   view: {
     flex: 1,
-    backgroundColor: "#ebebeb",
   },
-  map: {
-    flex: 1,
+  buttons: {
+    position: "absolute",
+    zIndex: 999,
+    top: 100,
+    left: 80,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 10,
   },
 });
